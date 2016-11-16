634cf4dff278bf54d792926111e8c6fd38b87521
Kat <katherineraurimcadams@gmail.com>
2016-11-16 11:24:23 -0800

    Added controllers for accounts, orders, and vendors with a view for each. Changed home controller to be the controller for inventory, so deleted contact and changed about to edit. Orders has partials for details, generate order, and index. Vendor has partials for edit and index.

--- PancakeCircus/Controllers/VendorController.cs
+++ PancakeCircus/Controllers/VendorController.cs
@@ -4,30 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace PancakeCircus.Controllers
 {
     [Route("[controller]")]
-    public class HomeController : Controller
+    public class VendorController : Controller
     {
         [HttpGet]
-        [HttpGet("/")]
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-
         [HttpGet("Edit")]
         public IActionResult Edit()
         {
-            //ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
-
-        [HttpGet("Error")]
-        public IActionResult Error()
-        {
             return View();
         }
     }
+     
 }
