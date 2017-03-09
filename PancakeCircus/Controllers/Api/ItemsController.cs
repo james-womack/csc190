0d73b09d8fe15914e0ad0983ace0496fa3492674
Marryum <malik@csus.edu>
2017-03-09 12:22:36 -0800

    Added the first item in the ItemController.

--- PancakeCircus/Controllers/Api/ItemsController.cs
+++ PancakeCircus/Controllers/Api/ItemsController.cs
@@ -12,37 +12,9 @@ namespace PancakeCircus.Controllers.Api
     [Route("api/[controller]")]
     public class ItemsController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public string Post([FromBody]AddItemRequest value)
-        {
-            return "hello James " + value.ItemName;
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        [HttpPut]
+        public IActionResult AddItem([FromBody]ItemObject item) {
+            return Ok();
         }
     }
 }
