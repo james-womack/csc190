a76fe7cd6da02a72c2107acf3f8369efbfde52e9
Marryum <malik@csus.edu>
2017-03-09 13:10:03 -0800

    Changes to ProductController

--- PancakeCircus/Controllers/Api/StockController.cs
+++ PancakeCircus/Controllers/Api/StockController.cs
@@ -17,17 +17,17 @@ namespace PancakeCircus.Controllers.Api
             return Json(new List<Stock>());
         }
         [HttpGet("vendor/{vendorId}")]
-        public IActionResult GetStock(int vendorId)
+        public IActionResult GetStock(string vendorId)
         {
             bool notFound = false;
             if (notFound )
             {
                 return NotFound();
             }
-            return Json(new List<Vendor>());
+            return Json(new List<Stock>());
         }
         [HttpGet("item/{itemId}")]
-        public IActionResult GetStock(int itemId)
+        public IActionResult GetItemStock(string itemId)
         {
             bool notfound = false;
             if (notfound)
@@ -36,8 +36,13 @@ namespace PancakeCircus.Controllers.Api
             }
             return Json(new List<Stock>());
         }
+        [HttpGet("low")]
+        public IActionResult GetLowStock()
+        {
+            return Json(new List<Stock>());
+        }
         [HttpGet]
-        public IActionResult GetStock([FromBody]Stock stock)
+        public IActionResult GetStock()
         {
             return Json(new List<Stock>());
         }
