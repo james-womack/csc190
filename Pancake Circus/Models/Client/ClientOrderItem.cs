cda7b6beaf1371fce72a74db2dc0f7535d211d46
Ashwin Sathyanarayan <ashwin1995@git>
2017-05-04 10:37:33 -0700

    Edited order items0

--- Pancake Circus/Models/Client/ClientOrderItem.cs
+++ Pancake Circus/Models/Client/ClientOrderItem.cs
@@ -8,19 +8,24 @@ namespace PancakeCircus.Models.Client
     {
     }
 
-    public ClientOrderItem(OrderItem orderItem)
+    public ClientOrderItem(OrderItem orderItem, Product product = null)
     {
       Item = new ClientItem(orderItem.Item);
       Vendor = new ClientVendor(orderItem.Vendor);
       Order = new ClientOrder(orderItem.Order, false);
       PaidPrice = orderItem.PaidPrice;
       OrderAmount = orderItem.OrderAmount;
+      if (product != null){
+        Product = new ClientProduct(product, orderItem.Item, orderItem.Vendor);
+      }
     }
 
     public ClientItem Item { get; set; }
     public ClientVendor Vendor { get; set; }
-    public ClientOrder Order { get; set; }
+    public ClientOrder Order { get; set; } 
+    public ClientProduct Product { get; set; }
     public int PaidPrice { get; set; }
     public int OrderAmount { get; set; }
+    
   }
 }
