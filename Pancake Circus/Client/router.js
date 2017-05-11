5091e6a05b439da17b4ce819eb8e36232c873022
James Womack <womackrjames@gmail.com>
2017-05-11 15:13:28 -0700

    fixed route bug

--- Pancake Circus/Client/router.js
+++ Pancake Circus/Client/router.js
@@ -28,7 +28,6 @@ export default new VueRouter({
         { path: 'orders', component: load('Orders') },
         { path: 'manage', component: load('Manage') },
         { path: 'inventory', component: load('Inventory') },
-        { path: 'vendorimport', component: load('VendorImport') },
         { path: 'login', component: load('Login') },
         { path: 'vieworder', component: load('ViewOrder') }
       ]
