466ebebf53bcba505da0c349d4e4940faaec1973
James Womack <womackrjames@gmail.com>
2017-05-10 19:59:48 -0700

    Added saving of all edits

--- Pancake Circus/Client/scripts/Manage.js
+++ Pancake Circus/Client/scripts/Manage.js
@@ -345,7 +345,9 @@ export default {
       newProductSelectionValid: false,
       // Stuff for actually adding/deleting/editing items
       criticalActionDone: false, // AKA added/deleted item or vendor
-      addRemovedProduct: false
+      addRemovedProduct: false,
+      // this is true when we are saving
+      saving: false
     }
   },
   methods: {
@@ -490,7 +492,7 @@ export default {
       this.criticalActionDone = true
 
       // Now add the item and clear out the field
-      itemsNew.push({
+      this.itemsNew.push({
         name: this.newItemName,
         minimumAmount: this.newItemMinAmount,
         units: this.newItemUnits
@@ -509,8 +511,8 @@ export default {
       // Set critical flag to true
       this.criticalActionDone = true
 
-      // Now add the vendor and clear out the fields
-      vendorsNew.push({
+      // Now add the vendor
+      this.vendorsNew.push({
         name: this.newVendorName,
         phone: this.newVendorPhoneNumber,
         streetAddress: this.newVendorStreetAddress,
@@ -520,6 +522,22 @@ export default {
         country: this.newVendorCountry
       })
 
+      // Add it to the table
+      let conv = this.converters.products([
+        {
+          name: this.newVendorName,
+          phone: this.newVendorPhoneNumber,
+          streetAddress: this.newVendorStreetAddress,
+          city: this.newVendorCity,
+          zipCode: this.newVendorZipCode,
+          state: this.newVendorState,
+          country: this.newVendorCountry
+      }])
+
+      conv[0].editStatus = this.statusTypes.new
+      this.vendorsData.push(conv[0])
+
+      // Clear out fields
       this.newVendorName = ''
       this.newVendorPhoneNumber = ''
       this.newVendorStreetAddress = ''
@@ -537,7 +555,7 @@ export default {
       // Set the added/removed flag
       this.addRemovedProduct = true
 
-      // Now add the product and clear out the fields
+      // Now add the product
       this.productsNew.push({
         itemId: this.newProductItem.id,
         vendorId: this.newProductVendor.id,
@@ -545,6 +563,26 @@ export default {
         price: this.newProductPrice,
         packageAmount: this.newProductPackageAmount
       })
+
+      // Add it to the table
+      let conv = this.converters.products([
+        {
+          item: this.newProductItem,
+          vendor: this.newProductVendor,
+          sku: this.newProductSku,
+          packageAmount: this.newProductPackageAmount,
+          price: this.newProductPrice
+      }])
+
+      conv[0].editStatus = this.statusTypes.new
+      this.productsData.push(conv[0])
+
+      // Clear out fields
+      this.newProductVendor = null
+      this.newProductItem = null
+      this.newProductSku = ''
+      this.newProductPackageAmount = ''
+      this.newProductPrice = ''
     },
     // Method to see if newProduct dialogs item and vendors are selected
     productIsVendorSelected () {
@@ -570,6 +608,169 @@ export default {
       })
 
       this.newProductSelectionValid = unique
+    },
+    // Saves the changes done to the manage page
+    saveChanges() {
+      /*
+      console.log('Item Changes')
+      console.log(this.itemsEdits)
+      console.log(this.itemsNew)
+      console.log(this.itemsDelete)
+
+      console.log('Vendor Changes')
+      console.log(this.vendorsEdits)
+      console.log(this.vendorsNew)
+      console.log(this.vendorsDelete)
+
+      console.log('Product Changes')
+      console.log(this.productsEdits)
+      console.log(this.productsNew)
+      console.log(this.productsDelete)
+      */
+
+      // Make requests pertainning to items now
+      this.saving = true
+
+      let itemsSave = Promise.resolve()
+      let itemsNew = Promise.resolve()
+      let itemsDelete = Promise.resolve()
+
+      if (this.itemsEdits.length > 0) {
+        itemsSave = this.$http.patch(ResolveRoute('items'), this.itemsEdits).then(resp => {
+          console.log('Items saved')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.itemsNew.length > 0) {
+        itemsNew = this.$http.put(ResolveRoute('items'), this.itemsNew).then(resp => {
+          console.log('New Items added')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.itemsDelete.length > 0) {
+        itemsDelete = this.$http.delete(ResolveRoute('items'), { body: this.itemsDelete }).then(resp => {
+          console.log('Items deleted')
+        })
+      }
+
+      // Now make requests pertainning to vendors
+      let vendorsSave = Promise.resolve()
+      let vendorsNew = Promise.resolve()
+      let vendorsDelete = Promise.resolve()
+
+      if (this.vendorsEdits.length > 0) {
+        vendorsSave = this.$http.patch(ResolveRoute('vendors'), this.vendorsEdits).then(resp => {
+          console.log('vendors saved')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.vendorsNew.length > 0) {
+        vendorsNew = this.$http.put(ResolveRoute('vendors'), this.vendorsNew).then(resp => {
+          console.log('New vendors added')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.vendorsDelete.length > 0) {
+        vendorsDelete = this.$http.delete(ResolveRoute('vendors'), { body: this.vendorsDelete }).then(resp => {
+          console.log('vendors deleted')
+        })
+      }
+
+      // Now make requests pertainning to products
+      let productsSave = Promise.resolve()
+      let productsNew = Promise.resolve()
+      let productsDelete = Promise.resolve()
+
+      if (this.productsEdits.length > 0) {
+        productsSave = this.$http.patch(ResolveRoute('products'), this.productsEdits).then(resp => {
+          console.log('products saved')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.productsNew.length > 0) {
+        productsNew = this.$http.put(ResolveRoute('products'), this.productsNew).then(resp => {
+          console.log('New products added')
+        }, err => {
+          console.log(err)
+        })
+      }
+
+      if (this.productsDelete.length > 0) {
+        productsDelete = this.$http.delete(ResolveRoute('products'), { body: this.productsDelete }).then(resp => {
+          console.log('products deleted')
+        })
+      }
+
+      // Combine into mega request
+      let all = Promise.all([itemsNew, itemsSave, vendorsDelete, vendorsNew, vendorsSave, vendorsDelete, productsNew, productsSave, productsDelete])
+
+      all.then(v => {
+        return this.refreshAll()
+      }, err => {
+        Toast.create('Failed to save changes')
+        this.saving = false
+      }).then(v => {
+        Toast.create('Saved Changes')
+        this.saving = false
+      }, err => {
+        console.log(err)
+        this.saving = false
+      })
+    },
+    discardChanges() {
+      // Discard changes
+      this.itemsEdits = []
+      this.itemsNew = []
+      this.itemsDelete = []
+      this.vendorsEdits = []
+      this.vendorsNew = []
+      this.vendorsDelete = []
+      this.productsEdits = []
+      this.productsNew = []
+      this.productsDelete = []
+
+      // Restore state
+      this.criticalActionDone = false
+      this.addRemovedProduct = false
+
+      // Refresh table
+      this.saving = true
+      this.refreshAll().then(v => {
+        this.saving = true
+      }, err => {
+        console.log(err)
+        this.saving = true
+      })
+    }
+  },
+  computed: {
+    changesMade: function () {
+      // Check new edits
+      if (this.itemsEdits.length > 0 || this.vendorsEdits.length > 0 || this.productsEdits.length > 0) {
+        return true
+      }
+
+      // Check for new entities
+      if (this.itemsNew.length > 0 || this.vendorsNew.length > 0 || this.productsNew.length > 0) {
+        return true
+      }
+
+      // Check for deletions
+      if (this.itemsDelete.length > 0 || this.vendorsDelete.length > 0 || this.productsDelete.length > 0) {
+        return true
+      }
+
+      return false
     }
   },
   watch: {
@@ -577,6 +778,13 @@ export default {
       this.newProductSelectionReady = this.productIsVendorSelected() && this.productIsItemSelected()
 
       // If there is a selection, ensure its a unique selection
+      if (this.newProductSelectionReady) {
+        this.newProductEnsureSelection ()
+      }
+    },
+    newProductVendor (newVendor) {
+      this.newProductSelectionReady = this.productIsVendorSelected() && this.productIsItemSelected()
+
       if (this.newProductSelectionReady) {
         this.newProductEnsureSelection ()
       }
